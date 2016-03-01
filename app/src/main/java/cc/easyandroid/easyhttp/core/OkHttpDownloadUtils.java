f424cdee3c3232b3b35d5c3bef024b0147da2e35
cgpllx <cgpllx1@qq.com>
2016-03-01 11:29:34 +0800

    fix bug

--- app/src/main/java/cc/easyandroid/easyhttp/core/OkHttpDownloadUtils.java
+++ app/src/main/java/cc/easyandroid/easyhttp/core/OkHttpDownloadUtils.java
@@ -26,7 +26,6 @@ public class OkHttpDownloadUtils extends OkHttpUtils {
      *
      * @param url
      * @param destFileDir 本地文件存储的文件夹
-     * @param callback
      */
     public void downloadAsyn(final String url, final String destFileDir) {
         final Request request = new Request.Builder().url(url).build();
@@ -55,6 +54,60 @@ public class OkHttpDownloadUtils extends OkHttpUtils {
                     while ((len = is.read(buf)) != -1) {
                         fos.write(buf, 0, len);
                     }
+                    fos.flush();
+                    // 如果下载文件成功，第一个参数为文件的绝对路径
+                    // sendSuccessResultCallback(file.getAbsolutePath(),
+                    // callback);
+                } catch (IOException e) {
+                    // sendFailedStringCallback(response.request(), e,
+                    // callback);
+                } finally {
+                    try {
+                        if (is != null)
+                            is.close();
+                    } catch (IOException e) {
+                    }
+                    try {
+                        if (fos != null)
+                            fos.close();
+                    } catch (IOException e) {
+                    }
+                }
+
+            }
+        });
+
+    }
+    public void downloadAsyn(final String url, final String destFileDir,final ProgressListener progressListener) {
+        final Request request = new Request.Builder().url(url).build();
+        final Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(final Request request, final IOException e) {
+                // sendFailedStringCallback(request, e, callback);
+            }
+
+            @Override
+            public void onResponse(Response response) {
+                InputStream is = null;
+                byte[] buf = new byte[2048];
+                int len = 0;
+                FileOutputStream fos = null;
+                try {
+                    is = response.body().byteStream();
+                    //得到数据的大小
+                    int length = is.available();
+                    File dir = new File(destFileDir);
+                    if (!dir.exists()) {
+                        dir.mkdirs();
+                    }
+                    File file = new File(dir, getFileName(url));
+                    fos = new FileOutputStream(file);
+                    while ((len = is.read(buf)) != -1) {
+                        fos.write(buf, 0, len);
+                        progressListener.update(len,length,false);
+                    }
+                    progressListener.update(len,length,true);
                     fos.flush();
                     // 如果下载文件成功，第一个参数为文件的绝对路径
                     // sendSuccessResultCallback(file.getAbsolutePath(),
