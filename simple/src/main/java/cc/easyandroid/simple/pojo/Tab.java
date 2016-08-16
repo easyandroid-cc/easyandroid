eea9c9a76d296f63f0117ef3d36fc1121a406330
cgpllx <cgpllx1@qq.com>
2016-08-16 10:18:02 +0800

    add tab.java

---
+++ simple/src/main/java/cc/easyandroid/simple/pojo/Tab.java
@@ -0,0 +1,23 @@
+package cc.easyandroid.simple.pojo;
+
+import cc.easyandroid.easydb.core.EasyDbObject;
+
+/**
+ * Created by cgp on 2016/6/12.
+ */
+public class Tab implements EasyDbObject {
+    @Override
+    public String buildKeyColumn() {
+        return xxx;
+    }
+
+    String xxx="d";
+
+    public String getXxx() {
+        return xxx;
+    }
+
+    public void setXxx(String xxx) {
+        this.xxx = xxx;
+    }
+}
